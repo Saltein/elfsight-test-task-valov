7154f65212005fbd175b352647d02a7fd9c969f5
Никита <nikitval@list.ru>
2025-11-14 21:28:37 +0300

    TextInput сохраняет значения в параметры URL

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export function TextInput({ placeholder }) {
-  return <StyledInput placeholder={placeholder} />;
+  const [currentText, setCurrentText] = useState();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setCurrentText(params.get(placeholder));
+  }, [placeholder]);
+
+  function onChange(e) {
+    const value = e.target.value;
+    const params = new URLSearchParams(window.location.search);
+    if (e.target.value) {
+      params.set(placeholder, value);
+    } else {
+      params.delete(placeholder);
+    }
+
+    window.history.replaceState({}, '', `?${params.toString()}`);
+    setCurrentText(value);
+  }
+
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      value={currentText}
+      onChange={onChange}
+    />
+  );
 }
 
 const StyledInput = styled.input`
